98d33f45445b118102f85254a53c477a47d83f81
Nitaa1904 <nitafitrotul1904@gmail.com>
2025-03-11 14:43:37 +0700

    Menambahkan method generic JumlahTigaAngka

---
+++ 03_GUI_Builder_dan_GitHub/modul5_2211104005/modul5_2211104005/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Penjumlahan
+{
+    // Method generic untuk menjumlahkan tiga angka
+    public static T JumlahTigaAngka<T>(T angka1, T angka2, T angka3)
+    {
+        dynamic a = angka1;
+        dynamic b = angka2;
+        dynamic c = angka3;
+
+        return a + b + c;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Input angka berdasarkan dua digit NIM: 22, 11, 10
+        int hasil = Penjumlahan.JumlahTigaAngka(22, 11, 10);
+
+        // Menampilkan hasil
+        Console.WriteLine($"Hasil Penjumlahan: {hasil}");
+    }
+}
