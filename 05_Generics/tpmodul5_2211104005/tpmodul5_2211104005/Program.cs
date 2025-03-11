080f281f99a0c267797cade4f5d682175abc8ce6
Nitaa1904 <nitafitrotul1904@gmail.com>
2025-03-12 01:20:27 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

---
+++ 05_Generics/tpmodul5_2211104005/tpmodul5_2211104005/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("[Nama Panggilan]");
+    }
+}
