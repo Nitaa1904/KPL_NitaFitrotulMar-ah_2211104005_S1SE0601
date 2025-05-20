f94de8d64cff84cea37cd11918e79a5d67136858
Nitaa1904 <nitafitrotul1904@gmail.com>
2025-05-20 16:21:29 +0700

    Feat: Add Movie model and MoviesController with CRUD operations

---
+++ 09_API_Design_dan_Construction_Using_Swagger/Jurnal/Movie.cs
@@ -0,0 +1,16 @@
+
+
+using System.Collections.Generic;
+
+namespace modul9_2211104005
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie() { }
+    }
+}
